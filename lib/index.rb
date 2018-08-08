7b1b78306f8594c521dc420b67fec72f5e853b33
George Schena <george@madetech.com>
2018-08-08 13:55:00 +0100

    invalid move working

--- lib/index.rb
+++ lib/index.rb
@@ -9,40 +9,47 @@ require 'human_plays'
 require 'ai_player'
 require 'minimax'
 
+before do
+  @memory = DiskBasedMemory.new
+  @view_grid = ViewGrid.new(game_state: @memory)
+end
+
 get '/' do
-  memory = DiskBasedMemory.new
-  response = ViewGrid.new(game_state: memory).execute({})
-  grid = memory.state
-  if memory.result == :winner
+  response = @view_grid.execute({})
+  grid = @memory.state
+  if @memory.result == :winner
     result = 'Computer wins'
-    memory.delete_all
-  elsif memory.result == :draw
+    @memory.delete_all
+  elsif @memory.result == :draw
     result = 'Draw'
-    memory.delete_all
+    @memory.delete_all
   end
-  erb :index, locals: { grid: grid, result: result }
+  erb :index, locals: { 
+    grid: grid, 
+    result: result,
+    invalid_move: params['error'] == 'invalid'
+  }
 end
 
 post '/make-move/:id' do
-  memory = DiskBasedMemory.new
-  update_grid = UpdateGrid.new(game_state: memory)
-  if end_of_game?(memory) || !memory.state[params[:id].to_i].nil?
-    return result = "Invalid Move"
+  update_grid = UpdateGrid.new(game_state: @memory)
+  if end_of_game?(@memory) || !@memory.state[params[:id].to_i].nil?
+    redirect '/?error=invalid'
   else
-    HumanPlayer.new(update_grid: update_grid, game_state: memory).plays(params[:id].to_i)
-    AIPlayer.new(update_grid: update_grid, game_state: memory).execute unless memory.result == :draw
+    HumanPlayer.new(update_grid: update_grid, game_state: @memory).plays(params[:id].to_i)
+    AIPlayer.new(update_grid: update_grid, game_state: @memory).execute unless @memory.result == :draw
   end
   redirect '/'
 end
 
 
 post '/reset' do
-  memory = DiskBasedMemory.new
-  memory.delete_all
+  @memory = DiskBasedMemory.new
+  @memory.delete_all
 
   redirect '/'
 end
 
 def end_of_game?(memory)
-  memory.result == :winner || memory.result == :draw
+  @memory.result == :winner || @memory.result == :draw
 end
